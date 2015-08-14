3a4f1437464deb0ce1d52aabd15f7fe0e1f0a3a2
Dmitry Gempler <DGempler@gmail.com>
2015-08-14 16:25:58 -0700

    added shuffle

--- script.js
+++ script.js
@@ -7,32 +7,46 @@
   var counter = 0;
   var clicked = [];
   var results = [];
+  var button = document.querySelector('button');
 
-  var images = {div1: 'url("https://boldlyproclaimingchrist.files.wordpress.com/2009/09/wolf.jpg?w=600")',
-  div5: 'url("https://boldlyproclaimingchrist.files.wordpress.com/2009/09/wolf.jpg?w=600")',
-  div2: 'url("http://cdn4.list25.com/wp-content/uploads/2011/11/dangerousanimals.png")',
-  div15: 'url("http://cdn4.list25.com/wp-content/uploads/2011/11/dangerousanimals.png")',
-  div3: 'url("http://www.planetdeadly.com/wp-content/uploads/grizzly-bear-420x347.jpg")',
-  div9: 'url("http://www.planetdeadly.com/wp-content/uploads/grizzly-bear-420x347.jpg")',
-  div4: 'url("http://www.hd-wallpapersdownload.com/upload/bulk-upload/ferocious-tiger-images.jpg")',
-  div10: 'url("http://www.hd-wallpapersdownload.com/upload/bulk-upload/ferocious-tiger-images.jpg")',
-  div6: 'url("http://www.freemalaysiatoday.com/wp-content/uploads/2013/12/Sun-Bears-300x224.jpg")',
-  div8: 'url("http://www.freemalaysiatoday.com/wp-content/uploads/2013/12/Sun-Bears-300x224.jpg")',
-  div7: 'url("http://www.awesomelycute.com/gallery/2011/09/awesomely-cute-animals-245.jpg")',
-  div13: 'url("http://www.awesomelycute.com/gallery/2011/09/awesomely-cute-animals-245.jpg")',
-  div14: 'url("http://i00.i.aliimg.com/wsphoto/v0/1904821646_1/ferocious-panther-diamond-painting-set-for-embroidery-diamond-cross-stitch-kit-Inlaid-decorative-painting-handmade-craft.jpg")',
-  div16: 'url("http://i00.i.aliimg.com/wsphoto/v0/1904821646_1/ferocious-panther-diamond-painting-set-for-embroidery-diamond-cross-stitch-kit-Inlaid-decorative-painting-handmade-craft.jpg")',
-  div12: 'url("http://3.bp.blogspot.com/-6EQqAX2Sqec/TvpEs6R6B8I/AAAAAAAAAZo/GJvvR4mO2eA/s1600/piranha+1.jpg")',
-  div11: 'url("http://3.bp.blogspot.com/-6EQqAX2Sqec/TvpEs6R6B8I/AAAAAAAAAZo/GJvvR4mO2eA/s1600/piranha+1.jpg")'
-  };
+  var images = {};
+  var imageArray = ['url("https://boldlyproclaimingchrist.files.wordpress.com/2009/09/wolf.jpg?w=600")',
+  'url("https://boldlyproclaimingchrist.files.wordpress.com/2009/09/wolf.jpg?w=600")',
+  'url("http://cdn4.list25.com/wp-content/uploads/2011/11/dangerousanimals.png")',
+  'url("http://cdn4.list25.com/wp-content/uploads/2011/11/dangerousanimals.png")',
+  'url("http://www.planetdeadly.com/wp-content/uploads/grizzly-bear-420x347.jpg")',
+  'url("http://www.planetdeadly.com/wp-content/uploads/grizzly-bear-420x347.jpg")',
+  'url("http://www.hd-wallpapersdownload.com/upload/bulk-upload/ferocious-tiger-images.jpg")',
+  'url("http://www.hd-wallpapersdownload.com/upload/bulk-upload/ferocious-tiger-images.jpg")',
+  'url("http://www.freemalaysiatoday.com/wp-content/uploads/2013/12/Sun-Bears-300x224.jpg")',
+  'url("http://www.freemalaysiatoday.com/wp-content/uploads/2013/12/Sun-Bears-300x224.jpg")',
+  'url("http://www.awesomelycute.com/gallery/2011/09/awesomely-cute-animals-245.jpg")',
+  'url("http://www.awesomelycute.com/gallery/2011/09/awesomely-cute-animals-245.jpg")',
+  'url("http://i00.i.aliimg.com/wsphoto/v0/1904821646_1/ferocious-panther-diamond-painting-set-for-embroidery-diamond-cross-stitch-kit-Inlaid-decorative-painting-handmade-craft.jpg")',
+  'url("http://i00.i.aliimg.com/wsphoto/v0/1904821646_1/ferocious-panther-diamond-painting-set-for-embroidery-diamond-cross-stitch-kit-Inlaid-decorative-painting-handmade-craft.jpg")',
+  'url("http://3.bp.blogspot.com/-6EQqAX2Sqec/TvpEs6R6B8I/AAAAAAAAAZo/GJvvR4mO2eA/s1600/piranha+1.jpg")',
+  'url("http://3.bp.blogspot.com/-6EQqAX2Sqec/TvpEs6R6B8I/AAAAAAAAAZo/GJvvR4mO2eA/s1600/piranha+1.jpg")'];
 
-function scoreTally() {
-  score.innerHTML = 'current score is ' + results.length / 2;
+  function shuffleImages() {
+    imageArray = _.shuffle(imageArray);
+    console.log(imageArray);
+    for (var i = 0; i < 17; i++) {
+      images["div" + (i+1)] = imageArray[i];
+      console.log(images);
+    }
 
-  if (results.length == 16) {
-    alert('YOU WON!');
   }
-}
+  shuffleImages();
+
+  function scoreTally() {
+    score.innerHTML = 'current score is ' + results.length / 2;
+
+    if (results.length == 16) {
+      alert('YOU WON!');
+    }
+  }
+
+
   (function board() {
 
     for (var i = 1; i < 17; i++) {
@@ -52,15 +66,15 @@ function scoreTally() {
       checker.addEventListener("click", function() {
         if (counter === 2) {
           console.log(results);
-         for (var i = 0; i < div.length; i ++) {
+          for (var i = 0; i < div.length; i ++) {
             div[i].style.backgroundImage = '';
-          for (var j = 0; j < results.length; j ++) {
-            if(div[i].getAttribute('id') === results[j]) {
-              div[i].style.backgroundImage = images[results[j]];
+            for (var j = 0; j < results.length; j ++) {
+              if(div[i].getAttribute('id') === results[j]) {
+                div[i].style.backgroundImage = images[results[j]];
+              }
             }
           }
-        }
-        counter = 0;
+          counter = 0;
         }
         else if (this.style.backgroundColor === "teal") {
           for (var key in images) {
@@ -76,15 +90,16 @@ function scoreTally() {
                   scoreTally();
                 } else {results.pop(); results.pop();
                   clicked = []; }
+                }
               }
             }
           }
-        }
-      });
-      container.appendChild(checker);
-    }
+        });
+  container.appendChild(checker);
+  }
   })();
-div = document.querySelectorAll('div');
+  div = document.querySelectorAll('div');
+
 
 })();
 
